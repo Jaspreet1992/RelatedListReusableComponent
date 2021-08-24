073eabb6478af48860f3aa97a2e790b2b1c60c44
Jaspreet1992 <Jaspreet.rulz92@gmail.com>
2021-08-24 23:03:40 +0530

    Update modalDialogGenerator.js

--- force-app/main/default/lwc/modalDialogGenerator/modalDialogGenerator.js
+++ force-app/main/default/lwc/modalDialogGenerator/modalDialogGenerator.js
@@ -1,3 +1,8 @@
+/*************************************************************************************************
+* This file is part of the apex-limit-framework project, released under the MIT License.             *
+* See LICENSE file or go to https://github.com/Jaspreet1992/RelatedListReusableComponents for full license details. *
+*************************************************************************************************/
+
 import { LightningElement, api } from 'lwc';
 
 export default class ModalDialogGenerator extends LightningElement {
